17ed4375e1fe9cb6c01a3c4d30e1469960776188
Tan Le <tle51@uic.edu>
2017-11-12 00:26:22 +0000

    screens set-up and service-worker done

---
+++ big-project/service-worker.js
@@ -0,0 +1,42 @@
+var cacheName = 'ctaApp-cache';
+var dataCacheName = 'cta-data-v1';
+var filesToCache = [
+  './',
+  './index.html',
+  './app.css'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
